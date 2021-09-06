105ada7a20e75248e2201c16e1b2880c8ad4aaf3
shinlooker <shinlooker@qq.com>
2021-09-06 16:11:59 +0800

    完善

--- app/src/main/java/com/shinlooker/demo/MainActivity.java
+++ app/src/main/java/com/shinlooker/demo/MainActivity.java
@@ -4,14 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.shinlooker.smartcard.R;
-import com.shinlooker.smartcard.SmartCard.OperationCallBack;
-import com.shinlooker.smartcard.SmartCard.ThreadPoolManager;
-import com.shinlooker.smartcard.SmartCard.core.CardResult;
-import com.shinlooker.smartcard.SmartCard.core.EnumReaderType;
-import com.shinlooker.smartcard.SmartCard.core.SmartCard;
-
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ThreadPoolManager.getInstance().execute(() -> {
-            CardResult execute = SmartCard.getInstance().execute("");
-        });
     }
 
-    public void Personal(String string, OperationCallBack<CardResult> callBack){
-
-    }
 }
